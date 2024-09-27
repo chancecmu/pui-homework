1a7912b97a9cc6af35d5de7b0bf62886df573a1b
chancecmu <chancec@andrew.cmu.edu>
2024-09-26 20:23:51 -0400

    hw4
    hw4

---
+++ solution-hw4/js/script.js
@@ -0,0 +1,97 @@
+//store basic price
+let basePrice;
+let cart = [];
+let newRoll;
+let rollType;
+
+//store glazes specs
+let allGlazes = [
+    { type: 'Original', add: 0 },
+    { type: 'Sugar Milk', add: 0 },
+    { type: 'Vanilla Milk', add: 0.50 },
+    { type: 'Double Chocolate', add: 1.50 },
+];
+
+//store pack specs
+let allPacks = [
+    { size: '1', multiply: 1 },
+    { size: '3', multiply: 3 },
+    { size: '6', multiply: 5 },
+    { size: '12', multiply: 10 },
+];
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing = rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+function updatePrice() {
+    //grab elements
+    let chosenGlaze = document.querySelector('#glazingOptions').value;
+    let chosenPack = document.querySelector('#packOptions').value;
+
+    //see if selection exists in class of glazes or pack, get associated price adaptation
+    //for .find: https://stackoverflow.com/questions/9907419/how-to-get-a-key-in-a-javascript-object-by-its-value
+
+    let glazePrice = allGlazes.find(glaze => glaze.type === chosenGlaze).add;
+    let packPrice = allPacks.find(pack => pack.size === chosenPack).multiply;
+
+    //calc total price based on price adaptation
+    let totalPrice = ((basePrice + glazePrice) * packPrice).toFixed(2);
+
+    //update the price by the add the cart button
+    let showPrice = document.querySelector('#totalPrice');
+    showPrice.innerText = "$ " + totalPrice;
+
+    newRoll = new Roll(rollType, chosenGlaze, chosenPack, totalPrice);
+}
+
+window.onload = function () {
+    let glazes = document.querySelector('#glazingOptions');
+    let packs = document.querySelector('#packOptions');
+
+    for (let i = 0; i < allGlazes.length; i++) {
+        var option = document.createElement('option');
+        option.text = allGlazes[i].type;
+        option.value = allGlazes[i].type;
+        glazes.add(option);
+    }
+
+    for (let i = 0; i < allPacks.length; i++) {
+        var option = document.createElement('option');
+        option.text = allPacks[i].size;
+        option.value = allPacks[i].size;
+        packs.add(option);
+    }
+
+    //relates to passed parameters from the detail links
+    const queryString = window.location.search; //get stuff after ?
+    const params = new URLSearchParams(queryString); //set up roll variable
+    rollType = params.get('roll'); //grab value inside of roll
+
+    // Update the header text
+    const headerElement = document.querySelector('#roll-header-text');
+    headerElement.innerText = rollType + " Cinnamon Roll";
+
+    // Update the image
+    const rollImage = document.querySelector('#roll-img');
+    rollImage.src = '../assets/products/' + rolls[rollType].imageFile;
+
+    // Update the base price
+    basePrice = rolls[rollType].basePrice;
+
+    updatePrice();
+
+    document.querySelector('#cartBtn').addEventListener('click', function () {
+        cart.push(newRoll);
+        console.log(cart);
+    });
+}
+
+
+
+
